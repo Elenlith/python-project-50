8ca0989f1849d06057849792186e2e7afd4833d9
Elenlith <mishchenkov@list.ru>
2023-01-13 23:20:04 +0300

    put the correct value for JSON files comparison test to the separate file

--- tests/test_get_difference.py
+++ tests/test_get_difference.py
@@ -1,8 +1,11 @@
 import json
+from pathlib import Path
 from gendiff.modules.get_difference import generate_diff
 
 
 def test_generate_diff():
     file_1 = json.load(open('../python-project-50/tests/fixtures/file1.json'))
     file_2 = json.load(open('../python-project-50/tests/fixtures/file2.json'))
-    assert generate_diff(file_1, file_2) == "{\n- age: 35\n+ birth_year: 1985\n- married: true\n+ married: false\n  name: Artyom\n- surname: Ivanov\n+ surname: Petrov\n}"
+    text = Path('../python-project-50/tests/fixtures/result.txt').read_text()
+    correct_answer = text[:-1]
+    assert generate_diff(file_1, file_2) == correct_answer
