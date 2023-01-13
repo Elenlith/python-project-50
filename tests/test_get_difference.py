ce83fd37c7ceeb53663cc85d290e5a263bebebc3
Elenlith <mishchenkov@list.ru>
2023-01-13 17:19:10 +0300

    modify paths to json files in the test

--- tests/test_get_difference.py
+++ tests/test_get_difference.py
@@ -3,6 +3,6 @@ from gendiff.modules.get_difference import generate_diff
 
 
 def test_generate_diff():
-    file_1 = json.load(open('~/python-project-50/tests/fixtures/file1.json'))
-    file_2 = json.load(open('~/python-project-50/tests/fixtures/file2.json'))
+    file_1 = json.load(open('../python-project-50/tests/fixtures/file1.json'))
+    file_2 = json.load(open('../python-project-50/tests/fixtures/file2.json'))
     assert generate_diff(file_1, file_2) == "{\n- age: 35\n+ birth_year: 1985\n- married: true\n+ married: false\n  name: Artyom\n- surname: Ivanov\n+ surname: Petrov\n}"
