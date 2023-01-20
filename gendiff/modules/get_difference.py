152d5fe3370808f4044634e731cfae0574ef4f7e
Elenlith <mishchenkov@list.ru>
2023-01-20 16:31:04 +0300

    extend functionality to compare the JSON files with the nested dicts

--- gendiff/modules/get_difference.py
+++ gendiff/modules/get_difference.py
@@ -3,12 +3,14 @@ def repr_el(elem):
         result = 'true'
     elif type(elem) is bool and elem is False:
         result = 'false'
+    elif elem is None:
+        result = 'null'
     else:
         result = elem
     return result
 
 
-def generate_diff(f_1, f_2):
+def make_diff(f_1, f_2):
     file1_keys = set(f_1.keys())
     file2_keys = set(f_2.keys())
     all_keys = list(file1_keys.union(file2_keys))
@@ -17,15 +19,43 @@ def generate_diff(f_1, f_2):
     added = file2_keys - file1_keys
     removed = file1_keys - file2_keys
     modified = {i: (f_1[i], f_2[i]) for i in sh_keys if f_1[i] != f_2[i]}
-    diff = '{'
+    unchanged = {i: (f_1[i], f_2[i]) for i in sh_keys if f_1[i] == f_2[i]}
+    diff = {}
     for i in all_keys:
         if i in added:
-            diff += str(f'\n+ {i}: {repr_el(f_2[i])}')
+            diff[('+ ' + i)] = f_2[i]
         elif i in removed:
-            diff += str(f'\n- {i}: {repr_el(f_1[i])}')
+            diff[('- ' + i)] = f_1[i]
+        elif i in unchanged:
+            diff[i] = f_1[i]
         elif i in modified:
-            diff += str(f'\n- {i}: {repr_el(f_1[i])}\n+ {i}: {repr_el(f_2[i])}')
-        else:
-            diff += str(f'\n  {i}: {repr_el(f_1[i])}')
-    diff += '\n}'
+            if type(f_1[i]) == dict and type(f_2[i]) == dict:
+                diff[i] = make_diff(f_1[i], f_2[i])
+            else:
+                diff[('- ' + i)] = f_1[i]
+                diff[('+ ' + i)] = f_2[i]
     return diff
+
+
+def stylish(diff, indent=1):
+    result = '{' + '\n'
+    for i in list(diff.keys()):
+        if i[0] == '+':
+            j = (indent - 1) * '    ' + '  + ' + i[2:]
+        elif i[0] == '-':
+            j = (indent - 1) * '    ' + '  - ' + i[2:]
+        else:
+            j = indent * '    ' + i
+        if type(diff[i]) is dict:
+            result += j + ': ' + stylish(repr_el(diff[i]), indent + 1) + '\n'
+        else:
+            result += j + ': ' + str(repr_el(diff[i])) + '\n'
+    if result[-1] == '}':
+        result += '\n' + (indent - 1) * '    ' + '}'
+    else:
+        result += (indent - 1) * '    ' + '}'
+    return result
+
+
+def generate_diff(f_1, f_2):
+    return stylish(make_diff(f_1, f_2))
--- tests/test_get_difference.py
+++ tests/test_get_difference.py
@@ -4,19 +4,19 @@ from pathlib import Path
 from gendiff.modules.get_difference import generate_diff
 
 
-def test_generate_diff_json():
-    file_1 = json.load(open('../python-project-50/tests/fixtures/file1.json'))
-    file_2 = json.load(open('../python-project-50/tests/fixtures/file2.json'))
-    text = Path('../python-project-50/tests/fixtures/result.txt').read_text()
-    correct_answer = text[:-1]
-    assert generate_diff(file_1, file_2) == correct_answer
-
-
-def test_generate_diff_yaml():
+def test_generate_diff_simple_yaml():
     with open('../python-project-50/tests/fixtures/file1.yml', 'r') as file:
         file_1 = yaml.safe_load(file)
     with open('../python-project-50/tests/fixtures/file2.yml', 'r') as file:
         file_2 = yaml.safe_load(file)
     t = Path('../python-project-50/tests/fixtures/result_yml.txt').read_text()
     correct_answer = t[:-1]
+    assert generate_diff(file_1, file_2) == correct_answer
+
+
+def test_generate_diff_adv_json():
+    file_1 = json.load(open('../python-project-50/tests/fixtures/file1.json'))
+    file_2 = json.load(open('../python-project-50/tests/fixtures/file2.json'))
+    text = Path('../python-project-50/tests/fixtures/result_j.txt').read_text()
+    correct_answer = text[:-1]
     assert generate_diff(file_1, file_2) == correct_answer
