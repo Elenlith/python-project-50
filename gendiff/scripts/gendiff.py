c578b48eb7b4d041bc0baf53c55e53113f1f0b8f
Elenlith <mishchenkov@list.ru>
2023-01-23 14:39:15 +0300

    indicate stylish as default format in the script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,7 +8,10 @@ def main():
     parser = argparse.ArgumentParser(description=text)
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument(
+        '-f', '--format', help='set format of output',
+        default='stylish'
+    )
     args = parser.parse_args()
 
     file1 = parse_data(args.first_file)
