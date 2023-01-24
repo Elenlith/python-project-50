004195577d49730d10bf2f4e0c49cb5fc1ccfcd7
Elenlith <mishchenkov@list.ru>
2023-01-24 21:52:23 +0300

    add test of __init__.py contents

---
+++ tests/test_gendiff.py
@@ -0,0 +1,14 @@
+import gendiff
+from gendiff.modules import get_difference
+
+
+def test_names():
+    assert hasattr(gendiff, 'generate_diff')
+
+
+def test_references():
+    assert gendiff.generate_diff is get_difference.generate_diff
+
+
+def test_all_metavar():
+    assert 'generate_diff' in gendiff.__all__
