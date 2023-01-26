688d06a400401dcea2c95e6febe9a0875445c846
Elenlith <mishchenkov@list.ru>
2023-01-26 13:24:34 +0300

    add test for YAML files comparison with plain output

--- tests/test_get_difference.py
+++ tests/test_get_difference.py
@@ -28,3 +28,13 @@ def test_generate_diff_json_plain():
     text = Path('../python-project-50/tests/fixtures/res_plain.txt').read_text()
     correct_answer = text[:-1]
     assert generate_diff(file_1, file_2, 'plain') == correct_answer
+
+
+def test_generate_diff_yaml_plain():
+    with open('../python-project-50/tests/fixtures/file1.yml', 'r') as file:
+        file_1 = yaml.safe_load(file)
+    with open('../python-project-50/tests/fixtures/file2.yml', 'r') as file:
+        file_2 = yaml.safe_load(file)
+    text = Path('../python-project-50/tests/fixtures/res_plain.txt').read_text()
+    correct_answer = text[:-1]
+    assert generate_diff(file_1, file_2, 'plain') == correct_answer
