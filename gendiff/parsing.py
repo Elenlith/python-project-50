aeef6d8648eddb8eb5e6a88e028908c5d4d065f7
Elenlith <mishchenkov@list.ru>
2023-03-27 09:54:26 +0300

    merge file parsing functions

--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -7,18 +7,10 @@ def read_file(filepath):
     return open(filepath, 'r')
 
 
-def parse_json(file_str):
-    return json.load(file_str)
-
-
-def parse_yaml(file_str):
-    return yaml.safe_load(file_str)
-
-
 def parse_data(filepath, file_str):
     ext = os.path.splitext(filepath)[-1].lower()
     if ext == '.json':
-        file = parse_json(file_str)
+        file = json.load(file_str)
     elif ext == '.yml' or '.yaml':
-        file = parse_yaml(file_str)
+        file = yaml.safe_load(file_str)
     return file
