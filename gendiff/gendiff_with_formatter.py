abb958fdbcbee8a2a4a401483bbeb518c6fb5b9b
Elenlith <mishchenkov@list.ru>
2023-03-28 13:15:57 +0300

    move format application to the diff to the separate function

--- gendiff/gendiff_with_formatter.py
+++ gendiff/gendiff_with_formatter.py
@@ -5,14 +5,21 @@ from gendiff.parsing import read_file, parse_data
 from gendiff.get_difference import make_diff
 
 
-def generate_diff(path_1, path_2, format='stylish'):
-    first_file_string = read_file(path_1)
-    second_file_string = read_file(path_2)
-    file_1 = parse_data(path_1, first_file_string)
-    file_2 = parse_data(path_2, second_file_string)
+def apply_format(file_1, file_2, format):
     if format == 'stylish':
         return stylish(make_diff(file_1, file_2))
     elif format == 'plain':
         return plain(make_diff(file_1, file_2))[:-1]
     elif format == 'json':
         return form_json(make_diff(file_1, file_2))
+    else:
+        raise NameError('Wrong format name')
+
+
+def generate_diff(path_1, path_2, format='stylish'):
+    first_file_string = read_file(path_1)
+    second_file_string = read_file(path_2)
+    file_1 = parse_data(path_1, first_file_string)
+    file_2 = parse_data(path_2, second_file_string)
+    result = apply_format(file_1, file_2, format)
+    return result
