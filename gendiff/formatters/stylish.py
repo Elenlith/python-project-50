b0007078a2ff298423e4ec6d707189222867c5b9
Elenlith <mishchenkov@list.ru>
2023-03-30 13:37:05 +0300

    update indent application logic in stylish

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,4 +1,4 @@
-def represent_item(item, indent=1):
+def represent_item(item, depth=1):
     result = ["{"]
     if isinstance(item, bool):
         return str(item).lower()
@@ -6,37 +6,44 @@ def represent_item(item, indent=1):
         return 'null'
     elif isinstance(item, dict):
         for key, val in item.items():
-            result.append(f"{'  ' * indent}  {key}: "
-                          f"{represent_item(val, indent + 2)}")
-        result.append(f"{'  ' * (indent - 1)}}}")
+            result.append(f"{make_indent(depth + 1)}{key}: "
+                          f"{represent_item(val, depth + 1)}")
+        result.append(f"{make_indent(depth)}}}")
     else:
         return item
     return '\n'.join(result)
 
 
-def build_stylish(diff, indent=1):
+def make_indent(depth, char=''):
+    if char:
+        return ' ' * (4 * depth - 2)
+    else:
+        return '    ' * depth
+
+
+def build_stylish(diff, depth=1):
     result = []
     for item in diff:
         status = item['type']
         key = item['key']
         if status == 'nested':
-            result.append(f"{'  ' * indent}  {key}: {{\n"
-                          f"{build_stylish(item['children'], indent + 2)}")
-            result.append(f"{'  ' * (indent + 1)}}}\n")
+            result.append(f"{make_indent(depth)}{key}: {{\n"
+                          f"{build_stylish(item['children'], depth + 1)}")
+            result.append(f"{make_indent(depth)}}}\n")
         elif status == 'changed':
-            result.append(f"{'  ' * indent}- {key}: "
-                          f"{represent_item(item['old_value'], indent + 2)}\n")
-            result.append(f"{'  ' * indent}+ {key}: "
-                          f"{represent_item(item['new_value'], indent + 2)}\n")
+            result.append(f"{make_indent(depth, '-')}- {key}: "
+                          f"{represent_item(item['old_value'], depth)}\n")
+            result.append(f"{make_indent(depth, '+')}+ {key}: "
+                          f"{represent_item(item['new_value'], depth)}\n")
         elif status == 'added':
-            result.append(f"{'  ' * indent}+ {key}: "
-                          f"{represent_item(item['value'], indent + 2)}\n")
+            result.append(f"{make_indent(depth, '+')}+ {key}: "
+                          f"{represent_item(item['value'], depth)}\n")
         elif status == 'deleted':
-            result.append(f"{'  ' * indent}- {key}: "
-                          f"{represent_item(item['value'], indent + 2)}\n")
+            result.append(f"{make_indent(depth, '-')}- {key}: "
+                          f"{represent_item(item['value'], depth)}\n")
         else:
-            result.append(f"{'  ' * indent}  {key}: "
-                          f"{represent_item(item['value'], indent + 2)}\n")
+            result.append(f"{make_indent(depth)}{key}: "
+                          f"{represent_item(item['value'], depth)}\n")
     result = ''.join(result)
     return result
 
